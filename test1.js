52669e4c07d239da56c312ced7830e0f2e1f53f7
vorasit phusuwan <40907778+vorasit@users.noreply.github.com>
2022-11-22 22:51:55 +0700

    backup

---
+++ test1.js
@@ -0,0 +1,77 @@
+// let prompt = require('prompt-sync')()
+// let a = prompt('input 1 >>');
+// let b = prompt('input 2 >>');
+// a = parseInt(a)
+// b = parseInt(b)
+// console.log(`sum = ${a+b}`);
+
+// function summation(...nums){
+//     var sum = 0;
+//     for(i=0; i<nums.length;i++){
+//         sum += nums[i];
+//     }
+//     return sum
+// }
+
+// var s = summation(1,2,3,4,5);
+// var a = [6,7,8,9];
+// console.log(s);
+// console.log(summation(...a));
+
+// var nums = [10,20,30,40];
+// var sum = 0;
+// for(i=0; i<nums.length; i++){
+//     sum += nums[i]
+// }
+// console.log(sum);
+
+
+// var browsers = new Array('chrome', 'edge', 'firefox', 'safari');
+// browsers.forEach(function(b,i){
+//     console.log(i+1, b);
+// })
+// browsers.forEach((b,i) => {
+//     console.log(i+1, b);
+// })
+
+/*
+let circle = {
+    radius: 10,
+    getArea : function(pi=3.14){
+        return pi * (this.radius ** 2)
+    },
+
+    getPerimeter(pi=3.14){
+        return 2*pi * this.radius
+    },
+}
+console.log(circle.getArea());
+console.log(circle.getPerimeter());
+*/
+
+/*
+let circle = {
+    radius: 10,
+    getArea : function(pi=3.14){
+        return pi * (this.radius ** 2)
+    },
+
+    getPerimeter(pi=3.14){
+        return 2*pi * this.radius
+    },
+}
+
+circle.getName = function(){
+    return 'circle'
+}
+
+circle.sphereVolume = (pi=3.14) => {
+    return (4/3) * pi * (circle.radius ** 3)
+}
+
+console.log(circle.getName());
+console.log(circle.sphereVolume());
+
+*/
+
+
